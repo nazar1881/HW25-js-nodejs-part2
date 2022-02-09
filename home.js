56e1a3b2c6e4c2fea96ec2c50c198fc21c63953b
nazar1881 <90273320+nazar1881@users.noreply.github.com>
2022-02-09 18:19:39 +0200

    first-commit

---
+++ home.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const port = 3000
+let url = require("url")
+let router = express.Router()
+
+let home = '/home'
+router.get(home, function(req, res, next) {
+    if(req.url == home) {
+        res.render('index', { title: 'Home page' });
+    } else {
+        next()
+    }    
+  });
+
+  module.exports = router
